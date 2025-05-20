5d362f453474c9054e99d14133e826e35f2e831e
Grace Contreras <95187728+Grarmy-171030@users.noreply.github.com>
2025-05-20 08:47:28 -0500

    Update Decimo_Ejercicio.py

--- Deber1/Decimo_Ejercicio.py
+++ Deber1/Decimo_Ejercicio.py
@@ -4,8 +4,8 @@
 # cada paquete a demanda. Cada payaso pesa 112 gy cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos 
 # en el último pedido y calcular el peso total del paquete que será enviado.
 peso_de_payaso = 112  # gramos
-peso_de_muñeca = 75   # gramos
+peso_de_muniecas = 75   # gramos
 numero_de_payaso = int(input("Ingrese el número de payasos: "))
-numero_de_muñeca = int(input("Ingrese el número de muñecas: "))
-peso_total = (numero_de_payaso * peso_de_payaso) + (numero_de_muñeca * peso_de_muñeca)
+numero_de_muniecas = int(input("Ingrese el número de muñecas: "))
+peso_total = (numero_de_payaso * peso_de_payaso) + (numero_de_muniecas * peso_de_muniecas)
 print("El peso total del paquete es:", peso_total, "gramos")
